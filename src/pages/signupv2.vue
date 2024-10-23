a554fed442b14e2b047486e5d4f499a771670e7b
whtan410 <whtan410@gmail.com>
2024-10-23 14:13:54 +0800

    signup page
    23/Oct

--- src/pages/signupv2.vue
+++ src/pages/signupv2.vue
@@ -3,43 +3,66 @@
         <div class="centered-div">
             <FormKit type="form">
                 <FormKit type="multi-step">
-                    <FormKit type="step" name="contactInformation">
+                    <FormKit type="step" name="Contact">
                         <!-- collect name, email, and company info -->
                         <FormKit 
                         type="text" 
-                        label="Your Name" 
-                        validation="required"
+                        label="First Name" 
                         />
                         <FormKit 
+                        type="text" 
+                        label="Last Name" 
+                        />
+                        <FormKit 
+                        type="text" 
+                        label="Phone number" 
+                        validation="required|regex:/^\+60\d{9}$/"
+                        validation-messages="{ regex: 'Phone number must be in the format +60xxxxxxxxx (e.g., +60123456789).' }"
+                        placeholder="+60123456789" />
+                        <FormKit 
                         type="email" 
-                        label="Your Email" 
+                        label="Email" 
                         validation="required|email" 
                         />
                         <FormKit 
-                        type="text" 
-                        label="Your Company (optional)"
-                        help="Are speaking on behalf of an organization?"
+                        type="password" 
+                        label="Password"
+                        validation="required"
                         />
                     </FormKit>
             
-                    <FormKit type="step" name="talkDetails">
+                    <FormKit type="step" name="Shipping">
                         <!-- Get talk title, brief, and track -->
                         <FormKit 
-                        type="text" 
-                        label="Talk Title" 
+                        type="textarea" 
+                        label="Street Address" 
                         validation="required"
                         />
                         <FormKit 
-                        type="textarea" 
-                        label="Talk Brief" 
+                        type="text" 
+                        label="City" 
                         validation="required|length:100"
-                        help="What is your talk about?"
                         />
                         <FormKit 
+                        type="number" 
+                        label="Postcode" 
+                        validation="required|length:100"
+                        />
+
+                        <FormKit
+                          type="select"
+                          name="framework"
+                          label="Choose your favorite frontend framework"
+                          :options="frameworks"
+                          input-class="custom-select"
+
+                        />
+
+                        <FormKit 
                         type="radio" 
-                        label="Talk Track"
-                        help="Which track are you submitting for?"
-                        :options="['Development', 'Testing', 'Leadership']"
+                        label="State"
+                        :options='["Malaysia"]'
+                        v-model="selectedState"
                         />
                     </FormKit>
                 </FormKit>
@@ -49,16 +72,58 @@
 </template>
 
 <script>
-//
+import { FormKit } from '@formkit/vue';
+
+export default {
+  components: { FormKit },
+  data() {
+    return {
+      selectedState: 'Malaysia',
+      frameworks : [
+        { label: 'Johor', value: 'johor' },
+        { label: 'Kedah', value: 'kedah' },
+        { label: 'Kelantan', value: 'kelantan' },
+        { label: 'Melaka', value: 'melaka' },
+        { label: 'Negeri Sembilan', value: 'negeri_sembilan' },
+        { label: 'Pahang', value: 'pahang' },
+        { label: 'Penang', value: 'penang' },
+        { label: 'Perak', value: 'perak' },
+        { label: 'Perlis', value: 'perlis' },
+        { label: 'Sabah', value: 'sabah' },
+        { label: 'Sarawak', value: 'sarawak' },
+        { label: 'Selangor', value: 'selangor' },
+        { label: 'Terengganu', value: 'terengganu' },
+        { label: 'Kuala Lumpur', value: 'kuala_lumpur' },
+        { label: 'Labuan', value: 'labuan' },
+        { label: 'Putrajaya', value: 'putrajaya'}
+      ]
+    };
+  },
+};
 </script>
 
-<!-- <style>
-.container {
-  display: grid;
-  place-items: center;
+<style>
+
+.formkit-input {
+  color: #ffffff; /* Set input text color to white */
+}
+
+.custom-select {
+  background-color: #000000; /* Change to desired color */
+}
+
+.formkit-form{
   width: 100%;
-  height: 100vh; /* This ensures the container takes up the full viewport height */
+}
+
+.centered-div {
+  display: flex;
+  justify-content: center; /* Horizontally center */
+  align-items: center;    /* Vertically center */
+  height: 100vh;          /* Full viewport height */
 }
 
 
-</style> -->
+</style>
+
+
