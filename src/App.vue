f357b5de274090ff63e665fcb5c3e134007e7be1
nic2456 <hoyonic2456@gmail.com>
2024-11-07 10:46:40 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <v-app>
+    <AppHeader />
     <v-main>
-      <router-view />
+      <router-view></router-view>
+    <AppFooter />
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts" setup>
-  //
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter,
+  }
+}
 </script>
+
+<style>
+/* Add any global styles here */
+</style>
