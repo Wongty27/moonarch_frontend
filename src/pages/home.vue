e98eddf2dc9053ed3e113f9f0db6a503090aba8b
nic2456 <hoyonic2456@gmail.com>
2024-11-07 10:51:50 +0800

    Update home.vue

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,40 +1,34 @@
 <template>
     <div class="container">
-      <ChildComponent :text="dynamicText1" :showButton="showButton1" :opac="opacity1" :wrapperHeight="wrapperheight1"/>
+      <GlitchGif :text="dynamicText1" :showButton="showButton1" :opac="opacity1" :wrapperHeight="wrapperheight1"/>
       <v-card>{{staticText1}}</v-card>
-      <ChildComponent :text="dynamicText2" :showButton="showButton2" :opac="opacity2" :wrapperHeight="wrapperheight2"/>
+      <GlitchGif :text="dynamicText2" :showButton="showButton2" :opac="opacity2" :wrapperHeight="wrapperheight2"/>
       <v-card>{{staticText2}}</v-card>
-      <ChildComponent :text="dynamicText3" :showButton="showButton3" :opac="opacity3" :wrapperHeight="wrapperheight3"/>
+      <GlitchGif :text="dynamicText3" :showButton="showButton3" :opac="opacity3" :wrapperHeight="wrapperheight3"/>
       <v-card>{{staticText3}}</v-card>
-      <
-      <v-card width=60vw>
-        <div class="d-flex ">
-            <div>
-                <h1 align="right">@ puchong vdfvfdvfvfdvdfvfdbranch</h1>
-                <h4 align="right"> adressline 1</h4>
-                <h4 align="right"> adressline 2</h4>
-                <h4 align="right"> adressline 2</h4>
-                <h4 align="right"> adressline 2</h4>
-                <h4 align="right"> HP: +60-xx xxx xxxx</h4>
-                <h4 align="right"> Email: puchongBranch@gmail.com</h4>
-            </div>
-            <v-avatar
-                class="ma-3"
-                rounded="0"
-                size="125"
-                >
-                    <v-img width="257" src="https://www.amerimacmanagement.com/wp-content/uploads/2021/11/Overhead-Map-of-Possible-Residential-Area.jpg"></v-img>
-            </v-avatar>
-        </div>
-      </v-card>
-      <
+      <div v-for="(location, index) in locations" 
+           :key="index"
+           class="map-container-wrapper">
+        <MapBox
+          :latitude="location.latitude"
+          :longitude="location.longitude"
+          :headline1="location.headline"
+          :add1="location.address1"
+          :add2="location.address2"
+          :add3="location.address3"
+          :hp1="location.phone"
+          :email1="location.email"
+          :index="index"
+        />
+      </div>
     </div>
 </template>
 
-  <script>
-  import ChildComponent from '../components/glitchgif.vue';
-  
-  export default {
+<script>
+import GlitchGif from '../components/glitchgif.vue';
+import MapBox from '../components/MapBox.vue';
+
+export default {
   name: 'GlitchEffect',
   data() {
     return {
@@ -57,12 +51,46 @@
       showButton3: false,
       wrapperheight3: '30vh',
       staticText3: '',
-    //end of modification section
+    
+      locations: [
+        {
+          latitude: 3.0333,
+          longitude: 101.6869,
+          headline: 'Puchong Branch',
+          address1: 'Address Line 1',
+          address2: 'Address Line 2',
+          address3: 'Address Line 3',
+          phone: '10-123 4567',
+          email: 'mnPuchong@gmail.com'
+        },
+        {
+          latitude: 3.1864,
+          longitude: 101.7059,
+          headline: 'Setapak Branch',
+          address1: 'Address Line 1',
+          address2: 'Address Line 2',
+          address3: 'Address Line 3',
+          phone: '10-765 4321',
+          email: 'mnSetapak@gmail.com'
+        },
+        {
+          latitude: 5.4141,
+          longitude: 100.3285,
+          headline: 'Penang Branch',
+          address1: 'Address Line 1',
+          address2: 'Address Line 2',
+          address3: 'Address Line 3',
+          phone: '12-567 1234',
+          email: 'mnPenang@gmail.com'
+        }
+        // Add more locations as needed
+      ]
     }
   },
   components: {
-    ChildComponent,
-  }
+    GlitchGif,
+    MapBox,
+  },
 };
 </script>
 
@@ -75,5 +103,17 @@
   justify-content: center;
   height: flex;
   overflow: hidden;
+}
+
+.map-container-wrapper {
+  margin-bottom: 50px; /* Adjust this value to control vertical spacing */
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+/* Remove margin from last container */
+.map-container-wrapper:last-child {
+  margin-bottom: 0;
 }
 </style>
