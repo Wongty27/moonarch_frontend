3d5ba66d64d8d489e400556ae993a7c860985c5d
nic2456 <hoyonic2456@gmail.com>
2024-11-07 10:41:22 +0800

    Delete src/pages/AppFooter.vue

--- src/pages/AppFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-footer class="d-flex">
-    Moonarch Dream Builders for Your Desires
-    <v-spacer />
-    @{{ new Date().getFullYear() }} Moonarch Tech PC SDN. BHD. All Rights Reserved.
-  </v-footer>
-</template>
